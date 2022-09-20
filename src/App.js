a08cac06524deb4eaf12d0d0125d63aa76a29994
kevanultroneous <dev21.ultroneous@gmail.com>
2022-09-20 09:17:57 +0530

    app js app css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Col, Row } from "react-bootstrap";
+import { BiSearch } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Row className="DetailRow">
+      <Col xl={12} lg={12}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LAST CHANCE to get 25% off our 16 Week Product Design course .
+          Flexible Learning. Live Mentorship. Hiring Connections. Sign up now!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Col>
+      <Col xl={12} lg={12}>
+        {[
+          "Discover",
+          "Animation",
+          "Branding",
+          "Illustration",
+          "Mobile",
+          "Print",
+          "Product Design",
+          "Typography",
+          "Web Design",
+        ].map((v, i) => (
+          <div key={i}>{v}</div>
+        ))}
+      </Col>
+
+      <Col xl={12} lg={12} className="DFlexCenter">
+        <h1 className="CenterHeading">
+          Explore the world's Leading design portfolios
+        </h1>
+        <p className="SubContent">
+          Millions of designers and agencies around the world showcase their
+          portfolio work on Dribbble - the home to the world’s best design and
+          creative professionals.
+        </p>
+        <div>
+          <div className="InputWrraper">
+            <BiSearch size={25} />
+            <input type="text" placeholder="Search..." />
+          </div>
+        </div>
+      </Col>
+      <Col xl={12} lg={12}>
+        <div>
+          <p>Trending searches</p>
+          {[
+            "Landing page",
+            "ios",
+            "food",
+            "landingpage",
+            "uxdesign",
+            "app design",
+          ].map((v, i) => (
+            <div key={i}>{v}</div>
+          ))}
+        </div>
+      </Col>
+    </Row>
   );
 }
 
